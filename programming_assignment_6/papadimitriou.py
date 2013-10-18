eb4cc54c76ba908eb29744f71ec938f123c4a484
Akeel <agilowen@gmail.com>
2013-10-18 01:04:41 -0400

    Fixed pre-processing step (tested on first dataset)

--- programming_assignment_6/papadimitriou.py
+++ programming_assignment_6/papadimitriou.py
@@ -3,86 +3,60 @@ lines = f.readlines()
 f.close()
 
 num_vars = int(lines[0])
-
+	
 clauses = []
-processed_clauses = []
-var_sign = {}
-
 for line in lines[1:]:
 	vars = line.split(' ')
 	var1, var2 = int(vars[0]), int(vars[1])
 
 	clauses.append((var1, var2))
 
-	if abs(var1) not in var_sign:
-		if var1 < 0:
-			var_sign[abs(var1)] = -1
-		else:
-			var_sign[abs(var1)] = 1
-	elif var_sign[abs(var1)] != -2:
-		if var1 < 0 and var_sign[abs(var1)] == 1:
-			var_sign[abs(var1)] = -2
-		elif var1 > 0 and var_sign[abs(var1)] == -1:
-			var_sign[abs(var1)] = -2
-	
-	if abs(var2) not in var_sign:
-		if var2 < 0:
-			var_sign[abs(var2)] = -1
-		else:
-			var_sign[abs(var2)] = 1
-	elif var_sign[abs(var2)] != -2:
-		if var2 < 0 and var_sign[abs(var2)] == 1:
-			var_sign[abs(var2)] = -2
-		elif var2 > 0 and var_sign[abs(var2)] == -1:
-			var_sign[abs(var2)] = -2
-	
+print "Number of clauses at start %d" % len(clauses)
+#Pre-processing Step to Reduce Clauses
+while True:
+	did_filter = False
+	var_sign = {}
+	processed_clauses = []
 
-print len(clauses)
+	for clause in clauses:
+		var1, var2 = clause[0], clause[1]
 
+		if abs(var1) not in var_sign:
+			if var1 < 0:
+				var_sign[abs(var1)] = -1
+			else:
+				var_sign[abs(var1)] = 1
+		elif var_sign[abs(var1)] != -2:
+			if var1 < 0 and var_sign[abs(var1)] == 1:
+				var_sign[abs(var1)] = -2
+			elif var1 > 0 and var_sign[abs(var1)] == -1:
+				var_sign[abs(var1)] = -2
 		
-for clause in clauses:
-	if ( (clause[0] not in var_sign) or (clause[0] in var_sign and var_sign[clause[0]] == -2) ) and \
-	   ( (clause[1] not in var_sign) or (clause[1] in var_sign and var_sign[clause[1]] == -2) ):
-		processed_clauses.append(clause)	
-	#add elifs to set the values of clause[0] or clause[1]
+		if abs(var2) not in var_sign:
+			if var2 < 0:
+				var_sign[abs(var2)] = -1
+			else:
+				var_sign[abs(var2)] = 1
+		elif var_sign[abs(var2)] != -2:
+			if var2 < 0 and var_sign[abs(var2)] == 1:
+				var_sign[abs(var2)] = -2
+			elif var2 > 0 and var_sign[abs(var2)] == -1:
+				var_sign[abs(var2)] = -2
+		
+	
+	for clause in clauses:
+		if ( (abs(clause[0]) not in var_sign) or (abs(clause[0]) in var_sign and var_sign[abs(clause[0])] == -2) ) and \
+		   ( (abs(clause[1]) not in var_sign) or (abs(clause[1]) in var_sign and var_sign[abs(clause[1])] == -2) ): 
+			processed_clauses.append(clause)	
+		else:
+			did_filter = True
+			#add elifs to set the values of clause[0] or clause[1]
+		
+	clauses = processed_clauses
 
+	if not did_filter:
+		break
+	
 
-#to_eliminate_global = []
-#for var in range(1, num_vars + 1):
-#	print "Processing %d from %d" % (var, num_vars)
-#	is_positive = 0
-#	to_eliminate_local = []
-#	to_be_eliminated = True
-#	for clause in clauses:
-#		if var == abs(clause[0]):
-#			if (clause[0] < 0 and is_positive == 1) or (clause[0] > 0 and is_positive == -1):
-#				to_be_eliminated = False
-#				break
-#			else:
-#				if (clause[0] < 0):
-#					is_positive = -1
-#				else:
-#					is_positive = 1
-#
-#				to_eliminate_local.append(clause)
-#
-#		elif var == abs(clause[1]):
-#			if (clause[1] < 0 and is_positive == 1) or (clause[1] > 0 and is_positive == -1):
-#				to_be_eliminated = False
-#				break
-#			else:
-#				if (clause[1] < 0):
-#					is_positive = -1
-#				else:
-#					is_positive = 1
-#
-#				to_eliminate_local.append(clause)
-#
-#	if to_be_eliminated:
-#		#SET value of variable forever (based on is_positive) or simply discard that variable
-#		to_eliminate_global += to_eliminate_local
-#
-#for clause in to_eliminate_global:
-#	clauses.remove(clause)
+print "Number of clauses after pre-processing %d" % len(clauses)
 
-print len(processed_clauses)
